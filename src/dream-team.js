51f878b58d624c5b5686da74e7977ecc553b9f9d
julia-fedulova <yuliua.fedulova@gmail.com>
2021-03-09 22:54:38 +0300

    feat: add count-cats and dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members === "undefined" || (!Array.isArray(members)) || members.length === 0) {
+    return false;
+  }
+
+  let teamName = [];
+
+  members.forEach((item) => {
+    if(typeof(item) === "string") {
+      str = item.trim();
+      teamName.push(str[0].toUpperCase());
+    }
+  });
+
+  return teamName.sort().join("");
 };
