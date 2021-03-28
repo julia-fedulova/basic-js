7c03860720f44027721ac4a8fc9dd03571592797
julia-fedulova <yuliua.fedulova@gmail.com>
2021-03-28 14:03:31 +0300

    feat: add vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
+  }
+
+  encrypt(message, key) {
+    let msg = message.toString().toUpperCase().split('');
+    let keys = key.toUpperCase().split('');
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let i = 0;
+    let arr;
+
+    if (!this.direction) {
+      msg = msg.reverse();
+      keys = keys.reverse();
+    }
+
+    arr = msg.map((item) => {
+      if (/^[\W\d]/.test(item)) {
+        return item;
+      }
+      let result = alphabet[(item.charCodeAt(0) + keys[i].charCodeAt(0)) % 26];
+      i++;
+      if (i === key.length) i = 0;
+      return result;
+    });
+
+    return arr.join('');
+  }
+
+  decrypt(message, key) {
+    let msg = message.toString().toUpperCase().split('');
+    let keys = key.toUpperCase().split('');
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let i = 0;
+    let arr;
+
+    if (!this.direction) {
+      msg = msg.reverse();
+      keys = keys.reverse();
+    }
+
+    arr = msg.map((item) => {
+      if (/^[\W\d]/.test(item)) {
+        return item;
+      }
+      let result = alphabet[(item.charCodeAt(0) + 26 - keys[i].charCodeAt(0)) % 26];
+      i++;
+      if (i === key.length) i = 0;
+      return result;
+    });
+
+    return arr.join('');
   }
 }
 
